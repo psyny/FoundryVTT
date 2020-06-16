647dddd237c447b70169bf66ce6e1852572184dd
Rafael França <psyny@hotmail.com>
2020-06-16 19:49:20 -0300

    First Push

---
+++ cozy-player/scripts/main.js
@@ -0,0 +1,195 @@
+// Automaticaly close sheets when rolling for something
+Hooks.once('ready', () => {
+  const originalMethod = Roll.prototype.toMessage;
+  Roll.prototype.toMessage = function (chatData={}, {rollMode=null, create=true}={}) {
+    closeSheets();
+    return originalMethod.apply(this, arguments);
+  };
+});
+
+Hooks.on('chatMessage', (chatLog, message, chatData) => {
+    let [command, match] = chatLog.constructor.parse(message);
+    if (!match) throw new Error("Unmatched chat command");
+
+    if(["roll", "gmroll", "blindroll", "selfroll"].includes(command)) {
+        closeSheets();
+        return true;
+    }
+});
+
+
+// Confort Shortcuts on default bar
+Hooks.on('getSceneControlButtons', controls => {
+	let control = controls.find(c => c.name === "token") || controls[0];
+	
+	control.tools.push({
+		name: "perception",
+		title: "Perception",
+		icon: "fas fa-eye",
+		visible: game.settings.get("cozy-player", "toolbarShowSkills"),
+		onClick: () => {
+      rollSkill('prc');
+      control.activeTool = "select";
+			return;
+		}
+	});
+  
+	control.tools.push({
+		name: "insight",
+		title: "Insight",
+		icon: "fas fa-brain",
+		visible: game.settings.get("cozy-player", "toolbarShowSkills"),
+		onClick: () => {
+      rollSkill('ins');
+      control.activeTool = "select";
+			return;
+		}
+	});
+	
+	control.tools.push({
+		name: "investigation",
+		title: "Investigation",
+		icon: "fas fa-search",
+		visible: game.settings.get("cozy-player", "toolbarShowSkills"),
+		onClick: () => {
+      rollSkill('inv');
+      control.activeTool = "select";
+			return;
+		}
+	});  
+  
+	control.tools.push({
+		name: "rollinitiative",
+		title: "Roll Initiative",
+		icon: "fas fa-flag-checkered",
+		visible: game.settings.get("cozy-player", "toolbarTurnTools"),
+		onClick: () => {
+      control.activeTool = "select";
+			enterCombatAndRollInitative();
+		}
+	});
+	
+	control.tools.push({
+		name: "endturn",
+		title: "End Turn",
+		icon: "fas fa-step-forward",
+		visible: game.settings.get("cozy-player", "toolbarTurnTools"),
+		onClick: () => {
+      control.activeTool = "select";
+			endTurn();
+		}
+	});
+});
+
+// ----------------------------------------------------------------------------------
+async function rollSkill(skillName)
+{
+	var controlled = canvas.tokens.controlled;
+	var actors = [];
+	var tgTkn;
+	
+	if( controlled.length == 0 ) controlled = canvas.tokens.ownedTokens;
+  
+  if( controlled.length == 0 ) {
+    actors.push( game.user.character );
+  } else {
+    for (let i = 0; i < controlled.length; i++) {
+      var tkn = controlled[i];
+      actors.push( tkn.actor );
+    }
+  }
+  
+  for (let i = 0; i < actors.length; i++) {
+    actors[i].rollSkill(skillName);
+  }
+}
+
+async function enterCombatAndRollInitative()
+{
+	
+	/* 
+	// Reference: how to create a combat... useful for when the player wants to start a combat
+	// WIP
+	if(!game.combat) {
+		let scene = game.scenes.viewed;
+		if ( !scene ) return;
+		let cbt = await game.combats.object.create({scene: scene._id});
+		await cbt.activate();
+	}
+	*/
+	
+	if(!game.combat && !game.user.isGM ) {
+		return;
+	}
+	
+	// Get Selected Tokens
+	var controlled = canvas.tokens.controlled;
+	var notInCombat = [];
+	var tgTkn;
+	
+	if( controlled.length == 0 )
+	{
+		controlled = canvas.tokens.ownedTokens;
+	}
+	
+	for (let i = 0; i < controlled.length; i++) {
+		var tkn = controlled[i];
+		if( !tkn.inCombat )
+		{
+			tgTkn = tkn;
+			notInCombat.push(tkn.id);
+		}
+	}
+	
+	
+	// If any token was marked, toggle combat.
+	// For some reason, all selected tokens will be toggled to combat...
+	if(tgTkn)
+	{
+		await tgTkn.toggleCombat();
+	}
+	
+	// Get combatants that was not in combat before
+	var toRoll = [];
+	for (let i = 0; i < notInCombat.length; i++) {
+			var tokenId = notInCombat[i];
+			var combatant = await game.combat.getCombatantByToken(tokenId);
+			if(combatant)
+			{
+				toRoll.push(combatant._id);
+			}
+	}
+	
+	// Roll for selected tokens that was not in combat before
+	if( toRoll.length > 0 )
+	{
+		game.combat.rollInitiative(toRoll);
+	}
+}
+
+// Close opened sheets
+async function closeSheets()
+{
+  if(!game.settings.get("cozy-player", "sheetsMinimizeOnRoll")) return;
+  
+	for(appId in ui.windows)
+	{
+		const win = ui.windows[appId];
+		if(win && win.options && win.options.baseApplication == "ActorSheet")
+		{
+			win.minimize();
+		}
+	}
+	return;
+}
+
+
+// Designed to player end current turn if its owner of current turn actor
+async function endTurn()
+{
+	var combatant = game.combat.combatant;
+	if(combatant && combatant.actor && combatant.actor.permission == ENTITY_PERMISSIONS["OWNER"] )
+	{
+		game.combat.nextTurn();
+	}
+}
---
+++ cozy-player/scripts/settings.js
@@ -0,0 +1,157 @@
+Hooks.once("init", () => {
+  game.settings.register("cozy-player", "sheetsMinimizeOnRoll", {
+		name: "Sheets: Minimize On Roll",
+    hint: "Minimize the character sheet when the user clicks on something that promps a roll",
+		scope: "client",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+  
+  game.settings.register("cozy-player", "toolbarShowSkills", {
+		name: "Left Toolbar: Show Skills",
+    hint: "Adds buttons to the left tool bar to roll perception, insight and investigation",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+  
+  game.settings.register("cozy-player", "toolbarTurnTools", {
+		name: "Left Toolbar: Turn Tools",
+    hint: "Adds buttons to the left tool bar to roll initiative and end turn",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+  
+	game.settings.register("cozy-player", "tooltipVisibility", {
+		name: "Token Tooltip: Visibility",
+		hint: "This option determines on which tokens to display a tooltip when moused over for players. The tooltip is always displayed for the GM.",
+		scope: "world",
+		config: true,
+		default: "owned",
+		type: String,
+		choices: {
+      "all": "All Tokens",
+      "observed": "Observed Tokens",
+      "friendly": "Friendly Tokens",
+      "ally": "Friendly or Observed Tokens",
+      "owned": "Owned Tokens",
+			"gm": "GM Only"
+		}
+	});
+  
+	game.settings.register("cozy-player", "tooltipScale", {
+		name: "Token Tooltip: Scale",
+		hint: "Size of the tooltip",
+		scope: "client",
+		config: true,
+		default: 1.0,
+    type: Number,
+    range: {min: 0.6, max: 2.0, step: 0.2}
+	});
+  
+	game.settings.register("cozy-player", "tooltipMaxItems", {
+		name: "Token Tooltip: Max Items",
+		hint: "Max number of informations to be shown in the tooltip.",
+		scope: "client",
+		config: true,
+		default: 21,
+    type: Number,
+    range: {min: 1, max: 40, step: 1}
+	});
+  
+	game.settings.register("cozy-player", "tooltipMaxRows", {
+		name: "Token Tooltip: Max Rows",
+		hint: "Max number of rows of informations. If this number is lower than the number of items, a new column is created.",
+		scope: "world",
+		config: true,
+		default: 7,
+    type: Number,
+    range: {min: 1, max: 20, step: 1}
+	});
+  
+  game.settings.register("cozy-player", "tooltipShowIcons", {
+		name: "Token Tooltip: Show Icons",
+    hint: "If an icon could not be found, the name of the feature will be used.",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+  
+	game.settings.register("cozy-player", "tooltipShowName", {
+		name: "Token Tooltip: Show Actor Name",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+  
+	game.settings.register("cozy-player", "tooltipShowHP", {
+		name: "Token Tooltip: Show Hit Points",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+ 
+	game.settings.register("cozy-player", "tooltipShowAC", {
+		name: "Token Tooltip: Show Armor Class",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	}); 
+  
+	game.settings.register("cozy-player", "tooltipShowSpeed", {
+		name: "Token Tooltip: Show Speed",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	}); 
+  
+	game.settings.register("cozy-player", "tooltipPassivePerception", {
+		name: "Token Tooltip: Show Passive Perception",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	}); 
+  
+	game.settings.register("cozy-player", "tooltipPassiveInsight", {
+		name: "Token Tooltip: Show Passive Insight",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	}); 
+  
+	game.settings.register("cozy-player", "tooltipResources", {
+		name: "Token Tooltip: Show Actor Resources",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	}); 
+  
+	game.settings.register("cozy-player", "tooltipFeats", {
+		name: "Token Tooltip: Show Featues",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	}); 
+  
+	game.settings.register("cozy-player", "tooltipFavs", {
+		name: "Token Tooltip: Show Tidy5esheet favorites",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	}); 
+  
+});
